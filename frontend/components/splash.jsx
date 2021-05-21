c9f0968edb7f0744939921111d5d2c2f9e971353
Carol Bares <caroltbares@gmail.com>
2021-05-21 05:19:01 -0700

    splash footer

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -29,11 +29,16 @@ class Splash extends React.Component {
         return (
             <div id='home-page'>
                 <div id='top-of-page'>
-                    <header>
+                    <header id='home-page-header'>
                         <Link id="homepage-logo-container" to='/login'>
                             <img id='homepage-logo' src={window.tlogo} alt="logo"/>
                             <img id='homepage-logo-text' src="https://fontmeme.com/permalink/210408/22f0e18bf49a6cbae65b62e7f5a1a78d.png" alt=""/>
                         </Link>
+                        <nav id='me-links'>
+                            <a href="https://www.linkedin.com/in/carol-twilight-bares-b4513694/" target="_blank"><i class="fab fa-linkedin"></i> Linkedin</a>
+                            <a href="https://github.com/Twiconoclast" target="_blank"><i class="fab fa-github-square"></i> Github</a>
+                            <a href="https://angel.co/u/carrie-bares" target="_blank"><i class="fab fa-angellist"></i> AngelList</a>
+                        </nav>
                         <div id='button-holder'>
                             <button id='login-button' onClick={this.handleLoginButton}>Login</button>
                         </div>
@@ -69,9 +74,41 @@ class Splash extends React.Component {
                     </div>
                 </div>
                 <footer>
-                    <nav id='home-footer-nav'></nav>
+                    <h2 id='about'>About the Developer</h2>
+                    <div id='bottom-of-page'>
+                        <nav id='project-nav'>
+                            <h4>Projects:</h4>
+                            <a href="https://cacophony-app.herokuapp.com/#/" target="_blank">Cacophony</a>
+                            <a href="https://budgethacks.herokuapp.com/#/" target="_blank">BudgetHacks</a>
+                            <a href="https://twiconoclast.github.io/MouseOnAMission/" target="_blank">Mouse on a Mission</a>
+                        </nav>
+                        <nav id='technologies'>
+                            <h4>Skills:</h4>
+                            <div>Ruby on Rails</div>
+                            <div>Javascript</div>
+                            <div>HTML</div>
+                            <div>CSS</div>
+                            <div>React</div>
+                            <div>Redux</div>
+                            <div>PostgreSQL</div>
+                            <div>MongoDB</div>
+                            <div>Express</div>
+                        </nav>
+                        <nav id='home-footer-nav'>
+                            <h4>Contact:</h4>
+                            <a href="https://www.linkedin.com/in/carol-twilight-bares-b4513694/" target="_blank"><i class="fab fa-linkedin"></i> Linkedin</a>
+                            <a href="https://github.com/Twiconoclast" target="_blank"><i class="fab fa-github-square"></i> Github</a>
+                            <a href="https://angel.co/u/carrie-bares" target="_blank"><i class="fab fa-angellist"></i> AngelList</a>
+                        </nav>
+                    </div>
                     <div id='subfooter'>
-                        <button id='footer-button' onClick={this.handleSigninButton}>Signup</button>
+                        <div id='sub-subfooter'>
+                            <Link id="footer-logo-container" to='/login'>
+                                <img id='homepage-logo' src={window.tlogo} alt="logo"/>
+                                <img id='homepage-logo-text' src="https://fontmeme.com/permalink/210408/22f0e18bf49a6cbae65b62e7f5a1a78d.png" alt=""/>
+                            </Link>
+                            <button id='footer-button' onClick={this.handleSigninButton}>Signup</button>
+                        </div>
                     </div>
                 </footer>
             </div>
